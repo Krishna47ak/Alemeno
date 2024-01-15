4f9ed73c64e6ae144b4285bfd308ec887ecb6577
Krishna47ak <anandakrishna720@gmail.com>
2024-01-16 02:48:48 +0530

    Created Navbar and CourseCard components in Home screen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import { useState } from "react"
+import { useSelector } from 'react-redux'
+import NavbarHeader from "../components/Navbar"
+import CourseCard from "../components/CourseCard"
+
 
 const Home = () => {
-  return (
-    <div className='bg-black text-white min-h-screen' >Home</div>
-  )
+    const [searchText, setSearchtext] = useState("")
+    const courses = useSelector(state => state?.courses?.courses)
+
+
+    return (
+        <div className='bg-black text-white min-h-screen' >
+            <NavbarHeader value={searchText} onChange={setSearchtext} />
+            <div className="grid grid-cols-2 gap-7 p-16" >
+                {courses?.map((course) => (
+                    <CourseCard key={course?.id} id={course?.id} img={course?.thumbnail} name={course?.name} description={course?.description} />
+                ))}
+            </div>
+        </div>
+    )
 }
 
 export default Home
